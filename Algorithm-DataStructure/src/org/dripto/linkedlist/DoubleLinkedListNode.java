aa90e51dc7f89d98adadb7d1a1b663e9e199cf02
Driptaroop Das <driptaroop.das@gmail.com>
2015-02-26 19:28:59 +0530

    Added double linked list node

---
+++ Algorithm-DataStructure/src/org/dripto/linkedlist/DoubleLinkedListNode.java
@@ -0,0 +1,13 @@
+package org.dripto.linkedlist;
+
+public class DoubleLinkedListNode {
+	
+	public int data;
+	public DoubleLinkedListNode nextNode;
+	public DoubleLinkedListNode previousNode;
+	
+	public DoubleLinkedListNode(int data){
+		this.data = data;
+	}
+	
+}
