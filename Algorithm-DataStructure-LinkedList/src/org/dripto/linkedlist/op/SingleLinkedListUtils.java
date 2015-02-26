edc5a8889b763390bf3fcedad59479c803d19a89
Driptaroop Das <driptaroop.das@gmail.com>
2015-02-26 19:57:54 +0530

    added util class

---
+++ Algorithm-DataStructure-LinkedList/src/org/dripto/linkedlist/op/SingleLinkedListUtils.java
@@ -0,0 +1,11 @@
+package org.dripto.linkedlist.op;
+
+import org.dripto.linkedlist.nodes.SingleLinkedListNode;
+
+public class SingleLinkedListUtils {
+	
+	public SingleLinkedListNode generateRandomList(int size){
+		return null;
+	}
+
+}
