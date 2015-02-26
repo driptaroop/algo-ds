b9a9064050c1184d573b3b1d1fa8d0be0fa5b581
Driptaroop Das <driptaroop.das@gmail.com>
2015-02-26 19:25:04 +0530

    Initial Changes

---
+++ Algorithm-DataStructure/src/org/dripto/linkedlist/SingleLinkedListNode.java
@@ -0,0 +1,11 @@
+package org.dripto.linkedlist;
+
+public class SingleLinkedListNode {
+	
+	public int data;
+	public SingleLinkedListNode nextNode;
+	
+	public SingleLinkedListNode(int data){
+		this.data = data;
+	}
+}
